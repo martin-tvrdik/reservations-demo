2c5e3ccc6412044d747c78ede29c5947da1f5a87
Martin Tvrdík <tvrdik.martin@gmail.com>
2023-04-18 21:44:53 +0200

    init repo

---
+++ src/test/java/org/mt/reservations/ReservationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mt.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
